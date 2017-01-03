8215b34530acb9d0f6e087cccad9779a3df03d4f
minatoismi <ismi1ion4arspada@gmail.com>
2017-01-04 05:26:15 +0700

    Simple Data Binding Final

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget3/ActivityMainBinding.java
@@ -0,0 +1,12 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget3;
+
+/**
+ * Created by sonic on 04/01/2017.
+ */
+public class ActivityMainBinding {
+    private String nama;
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget3/DataBindingUtil.java
@@ -0,0 +1,11 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget3;
+
+/**
+ * Created by sonic on 04/01/2017.
+ */
+public class DataBindingUtil {
+    public static ActivityMainBinding setContentView(MainActivity mainActivity, int activity_main) {
+
+        return null;
+    }
+}
